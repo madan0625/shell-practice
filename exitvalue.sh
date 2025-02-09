bce87fb78e255b74c0485a6c5731ac4a9f0d2c7e
madhu06 <madan.bnbcrypto@gmail.com>
2025-02-09 15:49:39 +0530

    v10 edit of this file

--- exitvalue.sh
+++ exitvalue.sh
@@ -3,15 +3,3 @@
 USERID=$(id -u)
 
 echo "your user id is : $USERID"
-
-echo "set to root :"
-
-apt list installed mysql
-
-if [ $? -ne 0 ]
- 
- then
-          echo "command is executed"
-          exit 1
-         
- fi
