71f1d271738e02033e020a24b0febcedeaf8bd87
madhu06 <madan.bnbcrypto@gmail.com>
2025-02-09 15:56:20 +0530

    v10 edit of this file

--- exitvalue.sh
+++ exitvalue.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+####################################################
+# this block just information of userid
+#########
+
 USERID=$(id -u)
 
 echo "your user id is : $USERID"
+
+
+#########
+
+# here using if-else condition 
+
+#############
+
+if [ $USERID -ne 0 ]
+
+then
+     echo "command is not executed"
+
+else
+      echo "comand is executed"
+
+ fi          
