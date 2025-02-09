5beaa1bd8a0837b85f01ec3007730eef7adfe1de
madhu06 <madan.bnbcrypto@gmail.com>
2025-02-09 21:23:44 +0530

    changes in code

--- student-marks.sh
+++ student-marks.sh
@@ -3,6 +3,7 @@
 SUBJECTS=("telugu" "hindi" "english" "maths" "science" "social")
 TOTALM=100
 i=6
+TOTAL=0
 
 for i in {0..5}
 do
@@ -10,6 +11,11 @@ echo "enter subject name : ${SUBJECTS[$i]}"
 echo "enter marks :"
 read MARKS
 echo "entered marks : $MARKS"
+
+TOTAL=$((TOTAL+MARKS))
+
+echo "total marks : $TOTAL" 
+
 #PERCENTAGE=$(echo "scale=2; ($MARKS/$TOTALM)*100" | bc)
 
 #echo "secured percentage is : $PERCENTAGE%"
