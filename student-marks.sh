33749307e5dd92e861ec7a81925ea07430fcc2db
madhu06 <madan.bnbcrypto@gmail.com>
2025-02-09 20:07:32 +0530

    new file is added

--- student-marks.sh
+++ student-marks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SUBJECTS=("telugu" "hindi" "english" "maths" "science" "social")
+TOTALM=100
 
 
 
@@ -8,3 +9,16 @@ echo "enter subject name : ${SUBJECTS[0]}"
 echo "enter marks :"
 read marks
 echo "entered marks : $marks"
+percentage=$(($marks/$TOTALM)100)
+
+echo "secured percentage is : $percentage"
+
+
+
+
+
+
+
+
+
+#perecentage = marks/ secured *100
