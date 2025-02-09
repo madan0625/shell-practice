b7e8493935625fbac6f8d09a0548fd7ba40b634b
madhu06 <madan.bnbcrypto@gmail.com>
2025-02-09 19:17:49 +0530

    a new file is created

--- readonly.sh
+++ readonly.sh
@@ -2,9 +2,9 @@
 
 # VAR1="madan"
 
-# readonly VAR1
+# readonly VAR1             # when you mentaion readonly before variable_name, it can't be changed by anyone
 
-# VAR1="kumar"
+# VAR1="kumar"              # it will prevent unwantedly over writing    
 
 VAR1="madan"
 
