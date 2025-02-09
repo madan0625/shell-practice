c415e24f0c2ccbbbaacc7594c0a35c4c58e03399
madhu06 <madan.bnbcrypto@gmail.com>
2025-02-09 16:12:22 +0530

    v10 edit of this file

--- exitvalue.sh
+++ exitvalue.sh
@@ -13,14 +13,17 @@ echo "your user id is : $USERID"
 
 # here using if-else condition 
 
+# generally user id is '1000', but in root previlages it is '0'
+
 #############
 
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ]                        # if user id 1000 then it is 1000 notequal 0 condition is true
+                                            # if user id  0, then the condition is 0 not equal 0. condition is false. else condition will execute
 
 then
      echo "command is not executed"
+     exit 1
 
-else
-      echo "comand is executed"
+ fi        
 
- fi          
+ echo "command is executed"  
