49ecc105c27460f859e73f5a78db74087cbc8fdc
madhu06 <madan.bnbcrypto@gmail.com>
2025-02-09 16:22:53 +0530

    v10 edit of this file

--- exitvalue.sh
+++ exitvalue.sh
@@ -17,12 +17,13 @@ echo "your user id is : $USERID"
 
 #############
 
-if [ $USERID -ne 0 ]                        # if user id 1000 then it is 1000 notequal 0 condition is true
+if [ $USERID -eq 0 ]                        # if user id 1000 then it is 1000 notequal 0 condition is true
                                             # if user id  0, then the condition is 0 not equal 0. condition is false. else condition will execute
 
 then
      echo "command is not executed"
-     exit 1
+     exit 1                                  # exit command is used to get the status of prvious command: 0 success, 1 failure
+                                             # if exit 1 - no command will run and exit from the script
 
  fi        
 
