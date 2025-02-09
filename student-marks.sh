a01c621970e6560cd132d51916a132c7ac723d4e
madhu06 <madan.bnbcrypto@gmail.com>
2025-02-09 21:48:00 +0530

    changes in code

--- student-marks.sh
+++ student-marks.sh
@@ -17,7 +17,7 @@ TOTAL=$((TOTAL+MARKS))
 done
 
 echo "total marks : $TOTAL"
-PERCENTAGE=$(echo "scale=2; ($TOTAL/$TOTALM)*100" | bc)
+PERCENTAGE=$(echo "scale=2; ($TOTAL/$TOTALM)*100")
 echo "secured percentage is : $PERCENTAGE"
 
 value=$PERCENTAGE
