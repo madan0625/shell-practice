728bf860311af44583e080190b3b2d63f5dbe53b
madhu06 <madan.bnbcrypto@gmail.com>
2025-02-09 19:37:49 +0530

    new file is added

--- args.sh
+++ args.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "no of arguments : $#"
+#echo "no of arguments : $#"
+
+
+if [ $# -gt 0 ]
+
+then
+    echo "no of args provided is : $#"
+
+ else
+    echo "no arg is provided"
+
+fi       
