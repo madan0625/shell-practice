4bc8ac206f611f446e89a7faf095c673d38f04a3
madhu06 <madan.bnbcrypto@gmail.com>
2025-02-09 14:25:18 +0530

    changes in file

--- exitvalue.sh
+++ exitvalue.sh
@@ -8,13 +8,13 @@ echo "set to root :"
 
 sudo su 
 
-# #if [ $USERID == 0 ]
+if [ $? -ne 0 ]
  
 #  if [ $USERID -ne 0 ]
  
-#  then
-#          echo "command is executed"
-#  else
-#          echo "command is not executed"
+ then
+          echo "command is executed"
+  else
+          echo "command is not executed"
          
 # fi
