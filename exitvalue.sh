b14f345e9df6fe87d7c187c4ee98ed5900a3b023
madhu06 <madan.bnbcrypto@gmail.com>
2025-02-09 16:43:06 +0530

    v10 edit of this file

--- exitvalue.sh
+++ exitvalue.sh
@@ -4,27 +4,52 @@
 # this block just information of userid
 #########
 
+# USERID=$(id -u)
+
+# echo "your user id is : $USERID"
+
+
+# #########
+
+# # here using if-else condition 
+
+# # generally user id is '1000', but in root previlages it is '0'
+
+# #############
+
+# if [ $USERID -ne 0 ]                        # if user id 1000 then it is 1000 notequal 0 condition is true
+#                                             # if user id  0, then the condition is 0 not equal 0. condition is false. else condition will execute
+
+# then
+#      echo "command is not executed"
+#      exit 1                                  # exit command is used to get the status of prvious command: 0 success, 1 failure
+#                                              # if exit 1 - no command will run and exit from the script
+
+#  fi        
+
+#block 1
+ ####################################################################################################
+
+
 USERID=$(id -u)
 
-echo "your user id is : $USERID"
-
-
-#########
-
-# here using if-else condition 
-
-# generally user id is '1000', but in root previlages it is '0'
-
-#############
-
-if [ $USERID -eq 0 ]                        # if user id 1000 then it is 1000 notequal 0 condition is true
-                                            # if user id  0, then the condition is 0 not equal 0. condition is false. else condition will execute
+if [ $USERID -ne 0 ]
 
 then
-     echo "command is not executed"
-     exit 1                                  # exit command is used to get the status of prvious command: 0 success, 1 failure
-                                             # if exit 1 - no command will run and exit from the script
+    echo "user switch to root privilages"
+    exit 1
 
- fi        
+fi    
 
- echo "command is executed"  
+apt list --installed git
+
+if [ $? -ne 0 ]
+
+then 
+     echo "installing the packages"
+     apt install git
+
+ else 
+      echo "git is already installed"
+
+ fi         
