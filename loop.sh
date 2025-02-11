252e7db9b6013fc287361af443ff619aa4d8d2ec
madhu06 <madan.bnbcrypto@gmail.com>
2025-02-11 16:58:47 +0530

    new file created

--- loop.sh
+++ loop.sh
@@ -2,9 +2,47 @@
 
 
 ## for loop
+# write a program to find odd & even unmbers
 
-for i in {0..1000}
+
+# for i in {0..1000}
+
+# do
+#       echo "no : $i"
+# done
+
+user_id=$(id -u)
+
+if [ $user_id -ne 0 ]
+
+then
+     echo "switch to root user"
+     exit 1
+fi         
+
+
+# to install a package or mltpile packages 
+
+for package in $@ 
 
 do
-      echo "no : $i"
+       apt list --installed $package
+       
+       if [ $? -ne 0 ]
+
+       then
+            echo "$package is not installed goint to install"
+            apt install $package
+            echo "$package is installed"
+
+            if [ $? -ne 0 ]
+
+            then
+                  echo "$package installation is not success"
+                  exit 1
+            else
+                  echo "$package installed successfully"     
+        else
+            echo "$package is already installed nothing to install it again!"
+        fi        
 done
