3a984361fbe161c27331be8bf2880a3cd688905e
madhu06 <madan.bnbcrypto@gmail.com>
2025-02-12 10:21:15 +0530

    new commit is added

--- pactice.sh
+++ pactice.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-LOG_FOLDER="/var/log/shell-practice"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIME_STAMP=$(date +%Y-%m-%D-%H:%M:%S-%A)
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
+log_folder="/var/log/shell-practice"
+script_name=$(echo $0 | cut -d "." -f1)
+time_stamp=$(date +%Y-%m-%D-%H:%M:%S-%A)
+log_file="$log_folder/$script_name-$time_stamp.log"
 
-mkdir -p $LOG_FOLDER
+mkdir -p $log_folder
 
 
 USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ]
 then
-     echo "switch to root previligaes to execute:" | tee -a $LOG_FILE
+     echo "switch to root previligaes to execute:" | tee -a $log_file
      exit 1
 fi
 
 
 
 USAGE(){
-          echo "USAGE:: no packages is paased - package1,package2..."
+          echo "USAGE:: no packages is paased - package1,package2..." | tee -a $log_file
           exit 1
 }
 
@@ -35,19 +35,19 @@ do
     if [ $? -eq 0 ]
 
     then
-         echo "$package is not installed going to install:" | tee -a $LOG_FILE
+         echo "$package is not installed going to install:" | tee -a $log_file
          apt install $package -y | tee -a $LOG_FILE
 
          if [ $? -ne 0 ]
          then
-             echo "$package installation status is unsuccessful:" | tee -a $LOG_FILE
+             echo "$package installation status is unsuccessful:" | tee -a $log_file
              exit 1
          else
-             echo "$package installation status is successful:" | tee -a $LOG_FILE
+             echo "$package installation status is successful:" | tee -a $log_file
          fi
 
     else
-         echo "$package is already installed nothing to install:" | tee -a $LOG_FILE
+         echo "$package is already installed nothing to install:" | tee -a $log_file
 
     fi                  
 done
