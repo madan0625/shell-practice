744ccc37e2927ac81091bcabb22738b8bca716ff
madhu06 <madan.bnbcrypto@gmail.com>
2025-02-13 19:27:35 +0530

    new file is added

--- findlogs.sh
+++ findlogs.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+R="\e[32m"
+G="\e[33m"
+N="\e[0m"
+
 SOURCE_DIR="/home/madan/logs"
 
 if [ -d $SOURCE_DIR ]
 
 then
-    echo "$SOURCE_DIR is exists:"
+    echo -e "$SOURCE_DIR $G is exists: $N"
 else
-    echo "$SOURCE_DIR is does not exists:"
+    echo "$SOURCE_DIR $R is does not exists: $N"
     exit 1
 fi    
